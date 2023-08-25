f450b3aac2d79ebac8f5cc66f4bc49459f94e300
ChrisuTheWeak <christian.olkkonen@metropolia.fi>
2023-08-25 10:00:15 +0300

    Working version of code here

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, FlatList, TouchableOpacity, Image, SafeAreaView} from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import List from './components/List';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+
+
+
+
+const App = () => {
+ return (
+  <>
+       <SafeAreaView style={styles.container}>
+         <List />
+       </SafeAreaView>
+       <StatusBar style="auto"/>
+     </>
+ );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ container: {
+   flex: 1,
+   backgroundColor: '#fff',
+   alignItems: 'center',
+   justifyContent: 'center',
+ },
 });
+
+export default App;
---
+++ components/Listitem.js
@@ -0,0 +1,27 @@
+
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import PropTypes from 'prop-types';
+
+const Listitem = ({singleMedia}) =>{
+return (
+  <TouchableOpacity
+  onPress={()=>{
+    console.log('Touched!', singleMedia.title);
+  }
+  }>
+  <Image
+    style={{width: 100, height: 100}}
+    source={{uri: singleMedia.thumbnails.w160}}
+  />
+  <View>
+    <Text>{singleMedia.title}</Text>
+    <Text>{singleMedia.description}</Text>
+  </View>
+</TouchableOpacity>
+)
+}
+
+Listitem.PropTypes = {
+  singleMedia: PropTypes.object,
+}
+export default Listitem;
